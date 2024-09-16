0212e1611422b2018ab392d2d2b70f78bf1463c7
BovineFreedo <frankskorstengaard@gmail.com>
2024-09-16 11:34:18 +0200

    implimented displayQuestion and subimtAnwser

---
+++ FlashCards2/src/frameworks/IQuestion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlashCards2.src.frameworks
+{
+    public interface IQuestion
+    {
+        public string[] displayQuestion();
+        public bool submitAnwser(bool[] anwsers);
+        public bool wasCorrect();
+    }
+}
---
+++ FlashCards2/src/implimentation/QuestionStub.cs
@@ -0,0 +1,36 @@
+﻿using FlashCards2.src.frameworks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlashCards2.src.implimentation
+{
+    public class QuestionStub : IQuestion
+    {
+        private string _question;
+        private List<IAnwser> _anwsers;
+        public QuestionStub(string question, List<IAnwser> anwsers) {
+            _question = question;
+            _anwsers = anwsers;
+        }
+        public List<string> displayQuestion()
+        {
+            List<string> display = new List<string>();
+            display.Add(_question);
+            return display;
+        }
+
+        public bool submitAnwser(List<bool> anwsers)
+        {
+            Console.WriteLine(anwsers[0]);
+            return true;
+        }
+
+        public bool wasCorrect()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

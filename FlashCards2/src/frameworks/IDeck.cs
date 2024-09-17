f2d57f948439b63b300a70eee47b6a4e6e24c4a5
BovineFreedo <frankskorstengaard@gmail.com>
2024-09-17 12:15:47 +0200

    made interface for the deck

---
+++ FlashCards2/src/frameworks/IDeck.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlashCards2.src.frameworks
+{
+    public interface IDeck
+    {
+        public List<string> nextQuestion();
+        public void submitAnswer();
+        public List<string> previousQuestion();
+    }
+}
